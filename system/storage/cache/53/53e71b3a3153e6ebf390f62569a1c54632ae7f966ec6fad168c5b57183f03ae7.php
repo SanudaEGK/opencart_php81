b63d60fe25774bbc2476fc15f249f196af13296a
Kulunu Sanuda <136562630+SanudaEGK@users.noreply.github.com>
2024-09-12 09:15:32 +0530

    make some changes

---
+++ system/storage/cache/53/53e71b3a3153e6ebf390f62569a1c54632ae7f966ec6fad168c5b57183f03ae7.php
@@ -0,0 +1,59 @@
+<?php
+
+/* default/template/common/column_left.twig */
+class __TwigTemplate_069ce4db260fed4287b0a56ea9b316b1e614e252842c56cf8b6e02cd24fa5756 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        if ((isset($context["modules"]) ? $context["modules"] : null)) {
+            // line 2
+            echo "<aside id=\"column-left\" class=\"col-sm-3 hidden-xs\">";
+            // line 3
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable((isset($context["modules"]) ? $context["modules"] : null));
+            foreach ($context['_seq'] as $context["_key"] => $context["module"]) {
+                // line 4
+                echo $context["module"];
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['module'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 6
+            echo "</aside>";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/column_left.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  33 => 6,  27 => 4,  23 => 3,  21 => 2,  19 => 1,);
+    }
+}
+/* {% if modules %}*/
+/* <aside id="column-left" class="col-sm-3 hidden-xs">*/
+/*   {% for module in modules %}*/
+/*   {{ module }}*/
+/*   {% endfor %}*/
+/* </aside>*/
+/* {% endif %}*/
+/* */
